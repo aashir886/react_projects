242502d1139d0c2b0a3e4d6ce6767cfbdfa1d9a8
aashir886 <70029091+aashir886@users.noreply.github.com>
2021-03-23 02:15:43 +0500

    created po1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react'
 import Navbar from './components/Layout/Navbar'
-import Users from './components/users/Users';
-import axios from 'axios';
+import Users from './components/users/Users'
+import Search from './components/users/Search'
+import Alert from './components/Layout/Alert'
+import axios from 'axios'
 import './App.css';
 
-export class App extends Component {
-
+class App extends Component {
   state = {
     users: [],
+    alert: null,
     loading: false
   }
 
- async componentDidMount () {
-   this.setState( { loading: true } );
-   const res = await axios.get( 'https://api.github.com/users' )
-   this.setState( { users: res.data } )
-   this.setState( { loading: false } );
-   console.log(res.data);
+  // this function is called from the search component by passing props
+  searchUsers = async (text) => {
+    this.setState( { loading: true } );
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState( { loading: false, users: res.data.items  } );
   }
+
+  // this func is called from the search component to the clear users
+  clearUsers = () => this.setState({ users: [], loading: false })
+
+  // this func is called from the serch component to raise an alert for empty text field
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } })
+    // remove the alert msg after 5s
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
+
+  clearAlert = () => {
+    this.setState({ alert: null })
+  }
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
-        <Navbar />
-        <div className="container">
-        <Users loading={ this.state.loading } users={ this.state.users } />
-        </div>
+       <Navbar  />
+       <div className="container" >
+       <Alert alert={ this.state.alert } />
+       <Search searchUsers={ this.searchUsers } 
+               clearUsers={ this.clearUsers } 
+               showClear={ users.length > 0 ? true : false }
+               setAlert={  this.setAlert}
+               showAlert={ this.state.alert !== null ? true : false  }
+               clearAlert={ this.clearAlert  }   
+        />
+       <Users loading={ loading } users={ users } />
+       </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-
 const Navbar = props => {
         return (
             <nav className="navbar bg-primary">
                 <h1>
                     <i className={ props.icon } />
-                    { props.title }
+                    {props.title}
                 </h1>
             </nav>
         )
     }
-    Navbar.defaultProps = {
-        title: 'Github finer',
-        icon: "fab fa-github"
-    }
 
-    Navbar.propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired,
-    }
+Navbar.defaultProps = {
+    title: "GitHub-Finder",
+    icon: "fab-fa-github"
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+}
 
 export default Navbar
-
